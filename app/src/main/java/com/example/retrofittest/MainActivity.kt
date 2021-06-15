848f7f6e895fdfe352f5da4e346996fd529fbb00
Stanislav Latyshev <postal915@gmail.com>
2021-06-15 16:13:03 +0300

    Display results in a RecyclerView

--- app/src/main/java/com/example/retrofittest/MainActivity.kt
+++ app/src/main/java/com/example/retrofittest/MainActivity.kt
@@ -1,10 +1,12 @@
 package com.example.retrofittest
 
 import android.os.Bundle
-import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.retrofittest.adapter.MyAdapter
 import com.example.retrofittest.databinding.ActivityMainBinding
 import com.example.retrofittest.repository.Repository
 import kotlinx.android.synthetic.main.activity_main.*
@@ -15,36 +17,30 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var viewModel: MainViewModel
 
+    private val myAdapter by lazy { MyAdapter() }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setupRecyclerView()
 
         val repository = Repository()
         val viewModelFactory = MainViewModelFactory(repository)
         viewModel = ViewModelProvider(this, viewModelFactory).get(MainViewModel::class.java)
 
-        val option: HashMap<String, String> = HashMap()
-        option["_sort"] = "id"
-        option["_order"] = "desc"
+        viewModel.getCustomPosts(2, "id", "desc")
+        viewModel.myCustomPosts.observe(this, Observer { response ->
+            if (response.isSuccessful) {
+                response.body()?.let { myAdapter.setData(it) }
+            } else {
+                Toast.makeText(this, response.code(), Toast.LENGTH_LONG).show()
+            }
+        })
+    }
 
-        button.setOnClickListener {
-            val myNumber = number_editText.text.toString()
-            viewModel.getCustomPosts2(myNumber.toInt(), option)
-            viewModel.myCustomPosts2.observe(this, Observer { response ->
-                if (response.isSuccessful) {
-                    textView.text = response.body().toString()
-                    response.body()?.forEach {
-                        Log.d("Response", it.userId.toString())
-                        Log.d("Response", it.id.toString())
-                        Log.d("Response", it.title)
-                        Log.d("Response", it.body)
-                        Log.d("Response", "----------")
-                    }
-                } else {
-                    Log.d("Response error", response.errorBody().toString())
-                    textView.text = response.code().toString()
-                }
-            })
-        }
+    private fun setupRecyclerView() {
+        recyclerView.adapter = myAdapter
+        recyclerView.layoutManager = LinearLayoutManager(this)
     }
 }
---
+++ app/src/main/java/com/example/retrofittest/adapter/MyAdapter.kt
@@ -0,0 +1,38 @@
+package com.example.retrofittest.adapter
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.retrofittest.R
+import com.example.retrofittest.model.Post
+import kotlinx.android.synthetic.main.row_layout.view.*
+
+class MyAdapter : RecyclerView.Adapter<MyAdapter.MyViewHolder>() {
+
+    private var myList = emptyList<Post>()
+
+    inner class MyViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyAdapter.MyViewHolder {
+        return MyViewHolder(
+            LayoutInflater.from(parent.context).inflate(R.layout.row_layout, parent, false)
+        )
+    }
+
+    override fun onBindViewHolder(holder: MyAdapter.MyViewHolder, position: Int) {
+        holder.itemView.userid_txt.text = myList[position].userId.toString()
+        holder.itemView.id_txt.text = myList[position].id.toString()
+        holder.itemView.title_txt.text = myList[position].title
+        holder.itemView.body_txt.text = myList[position].body
+    }
+
+    fun setData(newList: List<Post>) {
+        myList = newList
+        notifyDataSetChanged()
+    }
+
+    override fun getItemCount(): Int {
+        return myList.size
+    }
+}
